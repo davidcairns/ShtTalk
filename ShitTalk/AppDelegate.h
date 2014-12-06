5dbd84d2919c96f56d95ae1f3ece97f0bab2d5e3
David Cairns <drcairns@gmail.com>
2014-12-05 23:41:34 -0800

    Initial Commit

---
+++ ShitTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShitTalk
+//
+//  Created by David Cairns on 12/5/14.
+//  Copyright (c) 2014 David Cairns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShitTalk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShitTalk
+//
+//  Created by David Cairns on 12/5/14.
+//  Copyright (c) 2014 David Cairns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
